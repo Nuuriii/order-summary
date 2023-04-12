9374300eb58243202428efaf79ee4b6cb3c64407
nurr <nurr@pop-os.localdomain>
2023-04-12 21:46:58 +0700

    add background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Core from "./core/core";
+import Background from "../public/images/pattern-desktop.svg";
+import Background2 from "../public/images/pattern-mobile.svg";
 
 function App() {
    return (
-      <div className=" h-full pb-14 bg-blue-100">
+      <div
+         className=" h-full w- pb-14 bg-auto bg-indigo-200 bg-no-repeat"
+         style={{
+            backgroundImage: `url(${Background})`,
+         }}
+      >
          <Core />
       </div>
    );
